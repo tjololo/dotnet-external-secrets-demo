5f1a811170c9e296791e314bbf363427d01a4e0b
Vemund Gaukstad <tjololo@users.noreply.github.com>
2025-11-04 08:17:27 +0100

    Merge pull request #1 from tjololo/feat/init-app
    add a very simple app that featches not so secret secrets from disk a…

---
+++ dotnet-external-secrets-demo/Program.cs
@@ -0,0 +1,48 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/not-so-secret/from-disk", async () =>
+    {
+        const string secretFilePath = "/mnt/secrets-store/not-secret";
+        
+        if (!File.Exists(secretFilePath))
+        {
+            return Results.NotFound("Secret file not found on disk.");
+        }
+        var secret = await File.ReadAllTextAsync(secretFilePath);
+        var notSoSecretSecret = new NotSoSecretSecret("disk", secret);
+        return Results.Ok(notSoSecretSecret);
+    })
+    .WithName("get-not-so-secret-secret-from-disk");
+
+app.MapGet("/not-so-secret/from-environment", () =>
+    {
+        var secret = Environment.GetEnvironmentVariable("NOT_SO_SECRET_SECRET");
+        if (string.IsNullOrEmpty(secret))
+        {
+            return Results.NotFound("Environment variable 'NOT_SO_SECRET_SECRET' not found.");
+        }
+        var notSoSecretSecret = new NotSoSecretSecret("environment", secret);
+        return Results.Ok(notSoSecretSecret);
+    })
+    .WithName("get-not-so-secret-secret-from-environment");
+
+
+app.Run();
+
+record NotSoSecretSecret(string Source, string NotSecret)
+{
+}
